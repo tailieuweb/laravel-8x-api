47941802f0ea334f43d8771b606f419e82d7331a
thanh_nhuan <thanh_nhuan@lampart-vn.com>
2020-11-30 14:44:33 +0700

    Migration

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                'name' => "user{$i}",
+                'username' => "username{$i}",
+                'email' => "user{$i}@gmail.com",
+                'password' => Hash::make('123456789'),
+            ]);
+        }
+        
+    }
+}
