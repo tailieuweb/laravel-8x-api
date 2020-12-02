67c0226f64d9162c3b2f868efd25c25862042ee7
Nguyen Ngoc Truong <ngoctruong472k@gmail.com>
2020-12-02 15:05:33 +0700

    add the Phones table and the Phone_nums table

---
+++ database/migrations/2020_12_02_075539_create_phone_nums_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhoneNumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phone_nums', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('phone_id')->unsigned();
+            $table->string('phone_num');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phone_nums');
+    }
+}
