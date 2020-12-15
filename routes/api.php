0292221966c7e01776323bb57c2839acf0186e0d
thanh_nhuan <thanh_nhuan@lampart-vn.com>
2020-12-16 06:01:12 +0700

    Login example

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +20,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::post('register', [RegisterController::class, 'register']);
+Route::post('login', [RegisterController::class, 'login']);
+     
+Route::middleware('auth:api')->group( function () {
+    Route::resource('products', ProductController::class);
 });
