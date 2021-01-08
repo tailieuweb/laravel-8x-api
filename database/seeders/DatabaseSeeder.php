f950342236e0c39303111ee933b3914e37b161e7
TranPhongNha <44561836+TranPhongNha@users.noreply.github.com>
2021-01-08 23:19:52 +0700

    add seeder for check & checklist

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,31 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        for ($i = 1; $i <= 100; $i++) {
+            DB::table('checks')->insert([
+                'check_id' => $i,
+                'user_id' => 1,
+                'check_name' => "Bài {$i}: The name check is {$i}",
+                'check_overview' => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta con {$i}",
+                "check_description" =>  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta con {$i}",
+                'check_slug' => "the-name-is-check-{$i}",
+                'redmine_id' => "10{$i}",
+                'redmine_url' => "the-redmine-url-{$i}",
+                'check_status' => 99,
+                'created_at' =>  gmdate('Y-m-d H:i:s', time() + 7 * 3600),
+                'updated_at' =>  gmdate('Y-m-d H:i:s', time() + 7 * 3600),
+            ]);
+            for ($j = 1; $j <= 5; $j++) {
+                DB::table('checklists')->insert([
+                    'check_id' => $i,
+                    'user_id' => 1,
+                    'context_id' => 1,
+                    'context_type' => 'test',
+                    'status' => 99,
+                    'created_at' => gmdate('Y-m-d H:i:s', time() + 7 * 3600),
+                    'updated_at' =>  gmdate('Y-m-d H:i:s', time() + 7 * 3600),
+                ]);
+            }
+        }
     }
 }
