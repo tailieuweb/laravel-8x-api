ad4275ecef71ab0f415af600540cc5075c8fc8bd
tailieuweb <tailieuweb.com@gmail.com>
2021-05-07 17:01:06 +0700

    Dev passport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
+use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        //Passport
+        if (! $this->app->routesAreCached()) {
+            Passport::routes();
+            Passport::tokensExpireIn(Carbon::now()->addDays(1));
+            Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
+        }
     }
 }
